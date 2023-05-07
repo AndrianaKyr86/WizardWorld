a8c4ba73d329d098e89331ce158a2e88c3fb9bd1
Andy <andrianakyriaki@gmail.com>
2023-05-07 20:34:06 +0200

    First draft with pseudocode

--- wizard/src/App.jsx
+++ wizard/src/App.jsx
@@ -1,35 +1,146 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  let [houses, setHouses] = useState([]);
+
+  useEffect(() => {
+    fetch("")
+      .then((response) => response.json())
+      .then((data) => {
+        setHouses(data);
+      });
+  }, []);
+
+  function Navbar() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <span className="navbar-toggler-icon">
+            <a className="nav-link" href="#">
+              Home
+            </a>
+          </span>
+          <span className="navbar-text mx-3">
+            <a className="nav-link" href="#">
+              Houses
+            </a>
+          </span>
+          <span className="navbar-text mx-3">
+            <a className="nav-link" href="#">
+              Spells
+            </a>
+          </span>
+          <span className="navbar-text mx-3">
+            <a className="nav-link" href="#">
+              Potions
+            </a>
+          </span>
+        </div>
+      </nav>
+    );
+  }
+
+  // I want to insert four images of the four houses here
+  //when I click on the image, I want to display the house name, and description of the house
+  //I want to display the house name and description in a card and there should be a buttton to click to take you to the page Houses.jsx
+  //I need to import the Houses.jsx file
+
+  function Houses() {
+    return (
+      <div className="houses">
+        <div className="card">
+          <img
+            className="HouseImage" //I cannot resize the image in css
+            src="https://media.minalima.com/2019/02/option-1-gallery-02-gryffindor-house-crest-poster-scaled-1300x1300.jpg"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Gryffindor</h5>
+            <p className="card-text">
+              Gryffindor values courage, bravery, nerve, and chivalry.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            className="HouseImage"
+            src="https://media.minalima.com/2019/03/option-1-gallery-02-slytherin-house-crest-poster-scaled-1300x1300.jpg"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Slytherin</h5>
+            <p className="card-text">
+              Slytherin values ambition, cunning, leadership, and
+              resourcefulness.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            className="HouseImage"
+            src="https://media.minalima.com/2019/02/option-1-gallery-02-hufflepuff-house-crest-poster-scaled-1300x1300.jpg"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Hufflepuff</h5>
+            <p className="card-text">
+              Hufflepuff values hard work, patience, justice, and loyalty.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            className="HouseImage"
+            src="https://media.minalima.com/2019/02/option-1-gallery-02-ravenclaw-house-crest-poster-scaled-1300x1300.jpg"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Ravenclaw</h5>
+            <p className="card-text">
+              Ravenclaw values intelligence, creativity, learning, and wit.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div>
+      <Navbar />
+      <div className="paragraph">
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Welcome to Wizard World, the ultimate destination for aspiring witches
+          and wizards! Step inside and prepare to embark on an unforgettable
+          journey through the halls of Hogwarts School of Witchcraft and
+          Wizardry.
+        </p>
+        <p>
+          Get to know your fellow classmates and immerse yourself in the world
+          of Hogwarts. Learn all about the most famous witches and wizards from
+          each house, and discover their unique traits and accomplishments.
+          Whether you're a brave Gryffindor, a cunning Slytherin, a loyal
+          Hufflepuff, or a clever Ravenclaw, you'll find plenty of inspiration
+          and insight into the magic of the wizarding world.{" "}
+        </p>
+        <p>
+          And speaking of spells and potions, you won't want to miss our
+          comprehensive collection of magical recipes and incantations. From
+          beginner's charms to advanced hexes, our library has everything you
+          need to master the art of wizardry. Unleash your inner magic and
+          explore the vast collection of spells and potions at your fingertips.{" "}
+        </p>
+        <p>
+          Join us on a journey of discovery and exploration as we delve into the
+          rich history and lore of Hogwarts. You'll have the opportunity to
+          learn about the characters and stories that have captured the hearts
+          and imaginations of fans all over the world. From the daring exploits
+          of Harry Potter to the legendary achievements of Albus Dumbledore,
+          there's something for everyone to enjoy.
+        </p>
+        <p>
+          So come along and explore the wonders of Wizard World! You never know
+          what amazing discoveries and adventures lie ahead.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Houses />
+    </div>
+  );
 }
-
-export default App
---
+++ wizard/src/components/Houses.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "././characters:"; // I either need to find it in an API or recreate the json file to jsx
+
+//here I want to have the house name and description where the app.jsx will use for the featured house
+//I do not have a database with the house description but I have found description that I want to use
+//for each house.
+export default function Houses(props) {}
+
+//I want the handleClick to be here??
+// function handleClick()
+
+//I also want to pull the characters from the wizard db/data file according to the house
+// i.e. house = Gryffindor, characters = Harry Potter, Hermione Granger, Ron Weasley, etc.
+
+//Each House description
+// Gryffindor:
+// Known for their bravery, determination, and fierce loyalty, Gryffindors are the embodiment of courage. They are quick to act in the face of danger and will stop at nothing to protect their loved ones. Members of this house are often admired for their daring exploits, and their willingness to stand up for what is right, no matter the cost. With a lion as their emblem, Gryffindors are the ultimate champions of justice and honor.
+
+// Slytherin:
+// Cunning, ambitious, and fiercely independent, Slytherins are often misunderstood. They value self-preservation and will go to great lengths to achieve their goals. Members of this house are cunning strategists and skilled at outmaneuvering their opponents. With a serpent as their emblem, Slytherins embody the power of wisdom and the pursuit of greatness.
+
+// Hufflepuff:
+// Loyal, hardworking, and endlessly kind, Hufflepuffs are the backbone of the wizarding world. They believe in treating others with fairness and compassion, and they are always willing to lend a helping hand. Members of this house are often underestimated, but their unwavering dedication and strong sense of morality make them invaluable allies. With a badger as their emblem, Hufflepuffs represent the virtues of diligence, patience, and humility.
+
+// Ravenclaw:
+// Intellectual, curious, and endlessly creative, Ravenclaws are the ultimate seekers of knowledge. They value intelligence above all else and are always searching for new ways to expand their minds. Members of this house are often described as eccentric or quirky, but their unbridled passion for learning sets them apart from the rest. With an eagle as their emblem, Ravenclaws embody the power of knowledge and the pursuit of truth.
+
+// Each Hogwarts house is unique and special in its own way, and together they form the foundation of the wizarding world. So embrace your house and let your Hogwarts spirit shine!
