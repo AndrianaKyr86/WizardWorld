bf05fef559d0be13dfdccc2601fec2ef1f123afc
Andy <andrianakyriaki@gmail.com>
2023-05-18 11:14:33 +0200

    Added header and modified navbar

--- wizard/src/components/NavBar.jsx
+++ wizard/src/components/NavBar.jsx
@@ -3,29 +3,36 @@ import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="container-fluid">
-        <span className="navbar-toggler-icon">
-          <a className="nav-link" href="#">
-            Home
-          </a>
-        </span>
-        <span className="navbar-text mx-3">
-          <a className="nav-link" href="#">
-            Houses
-          </a>
-        </span>
-        <span className="navbar-text mx-3">
-          <a className="nav-link" href="#">
-            Spells
-          </a>
-        </span>
-        <span className="navbar-text mx-3">
-          <a className="nav-link" href="#">
-            Potions
-          </a>
-        </span>
-      </div>
+    // <nav className="navbar">
+    //   <div className="container-fluid">
+    //     <span className="navbar-toggler-icon">
+    //       <a className="nav-link" href="#">
+    //         Home
+    //       </a>
+    //     </span>
+    //     <span className="navbar-text mx-3">
+    //       <a className="nav-link" href="#">
+    //         Houses
+    //       </a>
+    //     </span>
+    //     <span className="navbar-text mx-3">
+    //       <a className="nav-link" href="#">
+    //         Spells
+    //       </a>
+    //     </span>
+    //     <span className="navbar-text mx-3">
+    //       <a className="nav-link" href="#">
+    //         Potions
+    //       </a>
+    //     </span>
+    //   </div>
+    // </nav>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Houses</a>
+      <a href="#">Spells</a>
+      <a href="#">Potions</a>
+      <div id="indicator"></div>
     </nav>
   );
 }
